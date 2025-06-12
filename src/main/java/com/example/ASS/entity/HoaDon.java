3a2e514b47ad5018bf7e5ca847fa5108f0953658
Bui Bao Quyen <buibaoquyen012@gmail.com>
2025-06-13 03:12:36 +0700

    sau khi da them dau du entity

---
+++ src/main/java/com/example/ASS/entity/HoaDon.java
@@ -0,0 +1,56 @@
+package com.example.ASS.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "HoaDon")
+public class HoaDon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Ma")
+    private String ma;
+
+    @Column(name = "NgayTao")
+    private LocalDate ngayTao;
+
+    @Column(name = "NgayThanhToan")
+    private LocalDate ngayThanhToan;
+
+    @Column(name = "NgayShip")
+    private LocalDate ngayShip;
+
+    @Column(name = "NgayNhan")
+    private LocalDate ngayNhan;
+
+    @Column(name = "TinhTrang")
+    private Integer tinhTrang;
+
+    @Column(name = "TenNguoiNhan")
+    private String tenNguoiNhan;
+
+    @Column(name = "DiaChi")
+    private String diaChi;
+
+    @Column(name = "Sdt")
+    private String sdt;
+
+    @ManyToOne
+    @JoinColumn(name = "IdKH", referencedColumnName = "id")
+    private KhachHang khachHang;
+
+    @ManyToOne
+    @JoinColumn(name = "IdNV", referencedColumnName = "id")
+    private NhanVien nhanVien;
+}
